2d39e38b3dadefcb9813c7436e91b7a9cbf7a5dc
fredchen90 <fredchen90@gmail.com>
2017-07-11 21:58:38 +0800

    Refactoring functional to class componets.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyATsNsLecO9z1uIbiDaV3vf5wKm_ga_A7M';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'something just like this'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
